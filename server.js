d02ce69ec9b349a475386576d79b6ba9bc00e6e5
Yoga Pratama Pangestu <64063549+yogapratama12428@users.noreply.github.com>
2024-05-10 08:00:43 +0700

    fix V1
    functional
    1. auth
    2. penghuni
    3. pegunjung

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import bcrypt from 'bcryptjs'
-import { PrismaClient } from '@prisma/client';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
@@ -10,93 +10,118 @@ const prisma = new PrismaClient();
 
 dotenv.config();
 
+app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post('/api/v1/register', async (req, res) => {
+app.get("/api/v1/penghuni", async (req, res) => {
+  try {
+    const pengunjung = await prisma.penghuni.findMany({
+      include: {
+        pengunjung: true,
+      },
+    });
 
-    const { email, name, password, alamat } = req.body;
+    res.status(200).json(pengunjung);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+});
 
-    try {
+app.post("/api/v1/pengunjung", async (req, res) => {
+  const { name, penghuniId } = req.body;
 
-        const salt = bcrypt.genSaltSync(10)
+  try {
+    const pengunjung = await prisma.pengunjung.create({
+      data: {
+        name,
+        status: "memanggil",
+        penghuniId,
+      },
+    });
 
-        const hashedPassword = bcrypt.hashSync(password, salt)
+    res.status(200).json(pengunjung);
+  } catch (error) {
+    console.log(error);
 
-        const response = await prisma.user.create({
-            data: {
-                email,
-                name,
-                alamat,
-                password: hashedPassword
-            }
-        })
+    res.status(500).json({
+      message: "some error occurred",
+    });
+  }
+});
 
-        res.status(200).json({
-            message: 'User created successfully'
-        });
+// MOBILE ROUTE
 
-    } catch (error) {
-        res.status(401).json(error);
+app.post("/api/v2/register", async (req, res) => {
+  const { email, name, password, alamat } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync(10);
+
+    const hashedPassword = await bcrypt.hashSync(password, salt);
+
+    await prisma.penghuni.create({
+      data: {
+        email,
+        name,
+        alamat,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(200).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+});
+
+app.post("/api/v2/sign-in", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const isValidEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!isValidEmail) {
+      return res.status(401).json({
+        message: "Invalid email",
+      });
     }
-})
 
-app.post('/api/v1/sign-in', async (req, res) => {
+    const isValidPassword = bcrypt.compareSync(password, isValidEmail.password);
 
-    const { email, password } = req.body;
-
-    try {
-        const isValidEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        })
-
-        if (!isValidEmail) {
-            return res.status(401).json({
-                message: 'Invalid email'
-            })
-        }
-
-        const isValidPassword = bcrypt.compareSync(password, isValidEmail.password)
-
-        if (!isValidPassword) {
-            return res.status(401).json({
-                message: 'Invalid password'
-            })
-        }
-
-        res.status(200).json({
-            message: 'Logged in successfully'
-        });
-
-    } catch (error) {
-        res.status(401).json(error);
+    if (!isValidPassword) {
+      return res.status(401).json({
+        message: "Invalid password",
+      });
     }
-})
 
+    res.status(200).json({
+      message: "Logged in successfully",
+    });
+  } catch (error) {
+    res.status(401).json(error);
+  }
+});
 
-app.post('/api/v2/pengunjung', async (req, res) => {
-    const { nama, jenis_kelamin, alamat } = req.body;
-    try {
-        const response = await prisma.pengunjung.create({
-            data: {
-                nama,
-                jenis_kelamin,
-                alamat
-            }
-        })
-        res.status(200).json({
-            message: 'Pengunjung created successfully'
-        });
-    } catch (error) {
-        res.status(401).json(error);
-    }
-})
+app.get("/api/v2/pengunjung", async (req, res) => {
+  try {
+    const pengunjung = await prisma.pengunjung.findMany({});
+
+    res.status(200).json(pengunjung);
+  } catch (error) {
+    res.status(404).json({ error: error });
+  }
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening at http://localhost:${process.env.PORT}`);
+  console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
